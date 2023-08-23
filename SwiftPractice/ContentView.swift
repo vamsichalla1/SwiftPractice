62d4387e3046f9fac8f1bfc8ad7272ae3f8f7958
vamsichalla1 <info2vams@gmail.com>
2023-08-23 15:36:41 -0400

    Initial Commit

---
+++ SwiftPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPractice
+//
+//  Created by Pravallika DineshReddy on 8/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftPractice/SwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPracticeApp.swift
+//  SwiftPractice
+//
+//  Created by Pravallika DineshReddy on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
